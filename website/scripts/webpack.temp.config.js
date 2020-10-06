7325e08fe2e2004261a63ee1d819d6dfe9e4c447
Niall Cargill <ikhnaton@techie.com>
2020-10-06 13:32:11 -0700

    add necessary "stuff" to allow deploy to Cloud Foundry (#8)

--- website/scripts/webpack.temp.config.js
+++ website/scripts/webpack.temp.config.js
@@ -1,21 +1,15 @@
 
 const path = require('path');
-const apiServer = 'https://fairchange-backend-embrace-fair-change.embrace-dev-ocp43-vpc-7ec5d722a0ab3f463fdc90eeb94dbc70-0000.us-east.containers.appdomain.cloud';
+const apiServer = process.env.API_SERVER;
+console.log({ apiServer });
 const webpack = require('webpack');
 const devConfig = {
-	devtool: 'source-map',
-	mode: 'development',
-	watch: true,
+	mode: 'production',
 	resolve: {
 		extensions: ['.js', '.jsx', '.ts', '.tsx'],
 		mainFields: ['browser', 'main', 'module']
-	},
-	stats: {
-		colors: true
 	}
 };
-
-const BFF = process.env.BFF_HOST || "http://localhost:9999";
 
 const port = process.env.PORT || 9000;
 
@@ -31,13 +25,6 @@ const clientConfig = Object.assign({}, prodConfig[0], devConfig, {
 		writeToDisk: true,
 		port: port,
 		proxy: {
-			// '/login': {
-			// 	target: apiServer,
-			// 	secure: false,
-			// 	logLevel: 'debug',
-			// 	changeOrigin: true,
-			// 	proxyTimeout: 60000
-			// },
 			'/api': {
 				target: apiServer,
 				secure: false,
